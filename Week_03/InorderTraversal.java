fba97c49824c2e322b68fceb81cc1f9c0603eea5
Piero Lin <linyouxie@millionser.com>
2020-09-08 19:12:37 +0800

    94. Binary Tree Inorder Traversal/二叉树的中序遍历

--- Week_03/InorderTraversal.java
+++ Week_03/InorderTraversal.java
@@ -1,2 +1,36 @@
-package algorithm015.Week_03;public class InorderTraversal {
+package algorithm015.Week_03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // 方法一：递归解法
+        /*
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+        */
+
+        // 方法二：迭代解法
+        List<Integer> list = new ArrayList<>();
+        return null;
+    }
+
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            helper(root.left, list);
+            list.add(root.val);
+            helper(root.right, list);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+    }
 }
