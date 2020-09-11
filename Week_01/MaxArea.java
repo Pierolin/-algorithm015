2ea7e3306284fbde27691b52c62e100826c1808c
piero <linyouxu@qq.com>
2020-09-11 15:06:21 +0800

    fix a small bug

--- Week_01/MaxArea.java
+++ Week_01/MaxArea.java
@@ -9,7 +9,7 @@ public class MaxArea {
         int max = 0;
         for (int i = 0, j = height.length - 1; i < j; ) {
             int minHeight = height[i] < height[j] ? height[i++] : height[j--];
-            max = Math.max(max, (j - i) * minHeight);
+            max = Math.max(max, (j - i -1) * minHeight);
         }
         return max;
 
