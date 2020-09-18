d1cf3f472ab12b14ac73ad8c43d14b7b343f0203
Piero Lin <linyouxie@millionser.com>
2020-09-18 10:01:57 +0800

    changes

--- Week_04/LemonadeChange.java
+++ Week_04/LemonadeChange.java
@@ -1,6 +1,14 @@
 package algorithm015.Week_04;
 
 public class LemonadeChange {
+    /**
+     * 贪心算法
+     * TC: O(n)
+     * SC: O(1)
+     * 关键思路：
+     * 1. 对收钱箱里的 5 元和 10 元分别实时计数判断是否找得开;
+     * 2. 收到 20 元时要优先使用 10+5 元来找，没有 10 元，再考虑用 5*3 来找，这就是所谓的“贪心算法”吗？
+     */
     public boolean lemonadeChange(int[] bills) {
         int bill5 = 0;
         int bill10 = 0;
