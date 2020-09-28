63c02fba2697f2a42683adbffab32cef6c5bb996
Piero Lin <linyouxie@millionser.com>
2020-09-28 17:59:52 +0800

    changes

--- Week_03/ReplaceSpace.java
+++ Week_03/ReplaceSpace.java
@@ -1,50 +1,69 @@
 package algorithm015.Week_03;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ReplaceSpace {
-    public String replaceSpace(String s){
-        // 方法一：使用新建 char 数组再转 String
+    public String replaceSpace_1(String s) {
 
+        /**
+         * 方法一：扩大数组
+         * TC: O(n)
+         * SC: O(n)
+         */
         int length = s.length();
-        char[] newS = new char[length*3];
+        char[] newS = new char[length * 3];
         int i = 0;
-        for(char c:s.toCharArray()){
-            if(c==' '){
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
                 newS[i++] = '%';
                 newS[i++] = '2';
                 newS[i++] = '0';
-            }
-            else{
+            } else {
                 newS[i++] = c;
             }
         }
-        return String.valueOf(newS,0,i);
+        return String.valueOf(newS, 0, i);
+    }
 
-        //方法二：使用 StringBuilder
-        /*
+    /**
+     * 方法二：使用 StringBuilder
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public String replaceSpace_2(String s) {
+
+
         StringBuilder sb = new StringBuilder();
-        for(char c:s.toCharArray()){
-            if(c == ' '){
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
                 sb.append("%20");
-            }
-            else{
+            } else {
                 sb.append(c);
             }
         }
         return sb.toString();
-        */
-
-        //方法三： 使用自带的 replaceAll 函数
-        //return s.replaceAll(" ","%20");
-
-
     }
+
+    public  String replaceSpace_3(String s){
+        List<String> list = new ArrayList<String>();
+        for (char c:s.toCharArray()) {
+            if (c == ' ') {
+                list.add("%20");
+            } else {
+                list.add(String.valueOf(c));
+            }
+        }
+        return String.join("",list);
+    }
+
+
 
     public static void main(String[] args) {
         ReplaceSpace replaceSpace = new ReplaceSpace();
         String str = "Hello world, Piero";
-        String result = replaceSpace.replaceSpace(str);
+        String result = replaceSpace.replaceSpace_3(str);
         System.out.println(result);
     }
 }
